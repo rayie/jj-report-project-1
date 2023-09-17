f0b587f6d16c3f323787b3d4faae263ca2ff7ca0
Raymond Ie <ie.raymond@gmail.com>
2023-09-16 23:05:51 -0700

    rie, added more comments in app.js

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
 const { authorize } = require("./authlib.js");
+
 /**
  * Prints the names and majors of students in a sample spreadsheet:
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
 async function list_records(auth) {
   //this authenticates the sheets api client
-  const sheets = google.sheets({version: 'v4', auth});
+  const sheets = google.sheets({ version: "v4", auth });
 
-
-  //this makes the api call to read data from the sheet. 
+  //this makes the api call to read data from the sheet.
   const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: '16mjKKM7yKNEXrBTJo1rzrW716Roz-gY3mh7CfvwYnLg',
-    range: 'Orders Data!A2:G',
+    spreadsheetId: "16mjKKM7yKNEXrBTJo1rzrW716Roz-gY3mh7CfvwYnLg",
+    range: "Orders Data!A2:G", //notice here that we are only asking for columns A to G, starting at row 2 (row 1 is the header)
   });
   const rows = res.data.values;
   if (!rows || rows.length === 0) {
-    console.log('No data found.');
+    console.log("No data found.");
     return;
   }
 
-
-
-  console.log('Name, PaymentMethod:');
-  rows.forEach((row) => {
+  console.log("Name, PaymentMethod:");
+  rows.forEach(row => {
     // Print columns A and E, which correspond to indices 0 and 4.
     console.log(`${row[1]}, ${row[6]}`);
   });
 }
 
-authorize().then(list_records).catch(console.error);
+async function run_using_async() {
+  let authClient = await authorize();
+  await list_records(authClient);
+}
 
+/*
+This code snippet defines a function run_using_promise that calls two other functions authorize and list_records using promises. 
+If authorize resolves successfully, it calls list_records. 
+If there is an error in either authorize or list_records, it logs the error to the console.
+*/
+function run_using_promise() {
+  authorize().then(list_records).catch(console.error);
+}
 
+//run_using_async();
 
+run_using_promise();
